73ee1fda26a3aa6a3218713e32a9bd922e13b8de
zenBen <benjamin.cowley@ttl.fi>
2018-09-20 08:52:01 +0300

    BC.

--- China/SCRIPTS/neuroenhance_branch_dev.m
+++ China/SCRIPTS/neuroenhance_branch_dev.m
@@ -48,9 +48,9 @@ OVERWRITE_OLD_RESULTS = false;
 
 
 %% Loop the available data sources
-for gix = 1:numel(group_dir)
+parfor gix = 1:numel(group_dir)
     sbj_filt = grpXsbj_filt{gix};
-    for pix = 1:numel(para_dir)
+    for pix = 3:numel(para_dir)
 %DEBUG:gix = 3; pix = 3;
 
         %Create the CONFIGURATION struct
@@ -71,7 +71,7 @@ for gix = 1:numel(group_dir)
                    @nebr_pipe3A,...
                    @nebr_pipe3B,...
                    @nebr_peekpipe};
-        runps = 2:6;
+        runps = 6;
         %You can also run only a subset of pipes, e.g. 2:length(pipeArr)
 
 
@@ -84,12 +84,12 @@ for gix = 1:numel(group_dir)
         end
 
 
-        %% Finally, obtain ERPs of known conditions from the processed data
+        %% Finally, compare pre-post improvements of stats for each branch
         % ...use CTAP_postproc_brancher helper function to rebuild branching
         % tree of paths to the export directories??
-        % CTAP_postproc_brancher(Cfg, @dynamic_func, {'loc_label', erploc}...
-        %                 , pipeArr, 'first', first, 'last', last...
-        %                 , 'dbg', STOP_ON_ERROR)
+        CTAP_postproc_brancher(Cfg, @dynamic_func, {'name', value}...
+                        , 'runPipes', runps...
+                        , 'dbg', STOP_ON_ERROR)
 
     end
 end
@@ -97,169 +97,3 @@ end
 %cleanup the global workspace
 clear PREPRO STOP_ON_ERROR OVERWRITE_OLD_RESULTS sbj_filt pipeArr first last
 
-
-%% Subfunctions
-
-% %% Return configuration structure
-% function [Cfg, out] = sbf_cfg(project_root_folder, ID)
-% 
-%     % Analysis branch ID
-%     Cfg.id = ID;
-%     Cfg.srcid = {''};
-%     Cfg.env.paths.projectRoot = project_root_folder;
-% 
-%     % Define important directories and files
-%     Cfg.env.paths.branchSource = ''; 
-%     Cfg.env.paths.ctapRoot = fullfile(Cfg.env.paths.projectRoot, Cfg.id);
-%     Cfg.env.paths.analysisRoot = Cfg.env.paths.ctapRoot;
-% 
-%     % Channel location file
-%     Cfg.eeg.chanlocs = Cfg.env.paths.projectRoot;
-% 
-%     % Define other important stuff
-%     Cfg.eeg.reference = {'average'};
-% 
-%     % NOTE! EOG channel specification for artifact detection purposes.
-%     Cfg.eeg.heogChannelNames = {'EXG1' 'EXG4'};
-%     Cfg.eeg.veogChannelNames = {'H24' 'EXG2'};
-% 
-%     % dummy var
-%     out = struct([]);
-% end
-
-
-% %% Configure pipe 1
-% function [Cfg, out] = sbf_pipe1(Cfg)
-% 
-%     %%%%%%%% Define hierarchy %%%%%%%%
-%     Cfg.id = 'pipe1';
-%     Cfg.srcid = {''};
-% 
-%     %%%%%%%% Define pipeline %%%%%%%%
-%     % Load
-%     i = 1; %stepSet 1
-%     stepSet(i).funH = { @CTAP_load_data,...
-%                         @CTAP_load_chanlocs,...
-%                         @CTAP_reref_data,... 
-%                         @CTAP_blink2event,...
-%                         @CTAP_fir_filter,...
-%                         @CTAP_run_ica };
-%     stepSet(i).id = [num2str(i) '_load'];
-% 
-%     out.load_chanlocs = struct(...
-%         'overwrite', true,...
-%         'delchan', 1,...
-%         'index_match', false);
-%     out.load_chanlocs.field = {{{'EXG1' 'EXG2' 'EXG3' 'EXG4'} 'type' 'EOG'}...
-%      , {{'EXG5' 'EXG6' 'EXG7' 'EXG8' '1EX5' '1EX6' '1EX7' '1EX8'} 'type' 'NA'}};
-%     out.load_chanlocs.tidy  = {{'type' 'FID'} {'type' 'NA'}};
-% 
-%     out.fir_filter = struct(...
-%         'locutoff', 1);
-% 
-%     out.run_ica = struct(...
-%         'method', 'fastica',...
-%         'overwrite', true);
-%     out.run_ica.channels = {'EEG' 'EOG'};
-% 
-% 
-%     %%%%%%%% Store to Cfg %%%%%%%%
-%     Cfg.pipe.runSets = {stepSet(:).id}; % step sets to run, default: whole thing
-%     Cfg.pipe.stepSets = stepSet; % record of all step sets
-% end
-
-
-% %% Configure pipe 2A
-% function [Cfg, out] = sbf_pipe2A(Cfg)
-% 
-%     %%%%%%%% Define hierarchy %%%%%%%%
-%     Cfg.id = 'pipe2A';
-%     Cfg.srcid = {'pipe1#1_load'};
-% 
-%     %%%%%%%% Define pipeline %%%%%%%%
-%     % IC correction
-%     i = 1;  %stepSet
-%     stepSet(i).funH = { @CTAP_detect_bad_comps,... %ADJUST for horiz eye moves
-%                         @CTAP_reject_data,...
-%                         @CTAP_detect_bad_comps,... %detect blink related ICs
-%                         @CTAP_filter_blink_ica,...
-%                         @CTAP_detect_bad_channels,...%bad channels by variance
-%                         @CTAP_reject_data,...
-%                         @CTAP_interp_chan };
-%     stepSet(i).id = [num2str(i) '_artifact_correction'];
-% 
-%     out.detect_bad_comps = struct(...
-%         'method', {'adjust' 'blink_template'},...
-%         'adjustarg', {'horiz' ''});
-% 
-%     out.detect_bad_channels = struct(...
-%         'method', 'variance',...
-%         'bounds', [-5; 2.5],...
-%         'take_worst_n', 2,...
-%         'channelType', {'EEG'}); %tune thresholds compared to basic pipe!
-%     
-%     %%%%%%%% Store to Cfg %%%%%%%%
-%     Cfg.pipe.runSets = {stepSet(:).id}; % step sets to run, default: whole thing
-%     Cfg.pipe.stepSets = stepSet; % record of all step sets
-% end
-
-
-% %% Configure pipe 2B
-% function [Cfg, out] = sbf_pipe2B(Cfg)
-% 
-%     %%%%%%%% Define hierarchy %%%%%%%%
-%     Cfg.id = 'pipe2B';
-%     Cfg.srcid = {'pipe1#1_load'};
-% 
-%     %%%%%%%% Define pipeline %%%%%%%%
-%     % IC correction
-%     i = 1;  %stepSet
-%     stepSet(i).funH = { @CTAP_detect_bad_comps,... %FASTER bad IC detection
-%                         @CTAP_reject_data,...
-%                         @CTAP_detect_bad_channels,...%bad channels by spectra
-%                         @CTAP_reject_data,...
-%                         @CTAP_interp_chan };
-%     stepSet(i).id = [num2str(i) '_artifact_correction'];
-% 
-%     out.detect_bad_comps = struct(...
-%         'method', 'faster',...
-%         'bounds', [-2.5 2.5],...
-%         'match_logic', @any);
-% 
-%     out.detect_bad_channels = struct(...
-%         'method', 'rejspec',...
-%         'channelType', {'EEG'});
-%     
-%     %%%%%%%% Store to Cfg %%%%%%%%
-%     Cfg.pipe.runSets = {stepSet(:).id}; % step sets to run, default: whole thing
-%     Cfg.pipe.stepSets = stepSet; % record of all step sets
-% end
-
-
-% %% Configure pipe for peeking at other pipe outputs
-% function [Cfg, out] = sbf_peekpipe(Cfg)
-% 
-%     %%%%%%%% Define hierarchy %%%%%%%%
-%     Cfg.id = 'peekpipe';
-%     Cfg.srcid = {'pipe1#1_load'...
-%                 'pipe1#pipe2A#1_artifact_correction'... 
-%                 'pipe1#pipe2B#1_artifact_correction'};
-% 
-%     %%%%%%%% Define pipeline %%%%%%%%
-%     i = 1; %next stepSet
-%     stepSet(i).funH = { @CTAP_peek_data };
-%     stepSet(i).id = [num2str(i) '_final_peek'];
-%     stepSet(i).save = false;
-% 
-%     out.peek_data = struct(...
-%         'secs', [10 30],... %start few seconds after data starts
-%         'peekStats', true,... %get statistics for each peek!
-%         'overwrite', false,...
-%         'plotAllPeeks', false,...
-%         'savePeekData', true,...
-%         'savePeekICA', true);
-% 
-%     %%%%%%%% Store to Cfg %%%%%%%%
-%     Cfg.pipe.stepSets = stepSet;
-%     Cfg.pipe.runSets = {stepSet(:).id};
-% end
