f6e473aaecfbf656d4dd0e8d37988dbfc248310e
zenBen <benjamin.cowley@ttl.fi>
2018-10-08 16:06:41 +0300

    Added parfor to main script to speed up.

--- China/SCRIPTS/report_stats.m
+++ China/SCRIPTS/report_stats.m
@@ -1,20 +1,44 @@
+%% INIT
 ind = '/media/ben/Maxtor/PROJECT_NEUROENHANCE/China/ANALYSIS/neuroenhance_base';
+oud = '/home/ben/Benslab/PROJECT_NEUROENHANCE/China/ANALYSIS';
 
-if exist(fullfile(ind, 'peek_stat_files.mat'), 'file') == 2
-    load(fullfile(ind, 'peek_stat_files.mat'))
+
+%% LOADING 
+if exist(fullfile(oud, 'peek_stat_files.mat'), 'file') == 2
+    load(fullfile(oud, 'peek_stat_files.mat'))
 else
-    peek_stats = subdir(fullfile(ind, 'peek_stats_log.xlsx'));
+    % This takes about 20 mins
+    peek_stat_files = subdir(fullfile(ind, 'peek_stats_log.xlsx'));
+    save(fullfile(oud, 'peek_stat_files.mat'), 'peek_stat_files')
 end
 
-T = cell(1, numel(peek_stats));
+tmp = fieldnames(peek_stat_files);
+tmp = cellfun(@(y) strrep(y, 'peekpipe/this/logs/peek_stats_log.xlsx', '')...
+    , cellfun(@(x) strrep(x, ind, '')...
+    , struct2cell(rmfield(peek_stat_files, tmp(2:end))), 'Un', 0), 'Un', 0);
+treeStats = cell2struct(tmp, 'pipename', 1);
 
-for pidx = 1:numel(peek_stats)
-    [isxl, xlsheets] = xlsfinfo(peek_stats(pidx).name);
-    xlsheets = xlsheets(~startsWith(xlsheets, 'Sheet'));
-    T{pidx} = cell(1, numel(xlsheets));
-    for sidx = 1:numel(xlsheets)
-        T{pidx}{sidx} = readtable(peek_stats(pidx).name...
-            , 'ReadRowNames', true...
-            , 'Sheet', xlsheets{sidx});
+if exist(fullfile(oud, 'peek_stats.mat'), 'file') == 2
+    load(fullfile(oud, 'peek_stats.mat'))
+else
+    % This takes about 6 hours! 'readtable()' takes a LONG time.
+    for pidx = 1:numel(peek_stat_files)
+        [isxl, xlsheets] = xlsfinfo(peek_stat_files(pidx).name);
+        xlsheets = xlsheets(~startsWith(xlsheets, 'Sheet'));
+        for sidx = 1:numel(xlsheets)
+            treeStats(pidx).pipe(sidx).sbjXpro = xlsheets{sidx};
+            treeStats(pidx).pipe(sidx).stat =...
+                readtable(peek_stat_files(pidx).name...
+                , 'ReadRowNames', true...
+                , 'Sheet', xlsheets{sidx});
+        end
     end
+    save(fullfile(oud, 'peek_stats.mat'), 'treeStats')
 end
+%read list of subjects per group
+sbjXpro = readtable(fullfile(ind, 'subjectXgroup.csv'));
+
+
+%% COMPARING
+
+p1_p2A3A = 
