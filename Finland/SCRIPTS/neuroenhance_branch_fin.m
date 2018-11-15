6fd0abcf09fa7c07868ef37816bba064bd2df257
zenBen <benjamin.cowley@ttl.fi>
2018-11-15 13:10:09 +0200

    developing finnish pipeline.

--- Finland/SCRIPTS/neuroenhance_branch_fin.m
+++ Finland/SCRIPTS/neuroenhance_branch_fin.m
@@ -48,13 +48,13 @@ ctapID = {'pre' 'post'};
 ctapID = ctapID{1};%PICK YOUR TIMEPOINT HERE! PRE or POST...
 
 % use sbj_filt to select all (or a subset) of available recordings
-grpXsbj_filt = {[130 132] [] [] []}; %setdiff(1:12, [3 7]);
+grpXsbj_filt = {[-130 -132] [] [] []}; %setdiff(1:12, [3 7]);
 
 % Runtime options for CTAP:
-DEBUG = true;
+DEBUG = false;
 PREPRO = true;
-STOP_ON_ERROR = true;
-OVERWRITE_OLD_RESULTS = false;
+STOP_ON_ERROR = false;
+OVERWRITE_OLD_RESULTS = true;
 
 
 %% Loop the available data sources
@@ -64,13 +64,13 @@ if DEBUG
 else
   parforArg = Inf;
 end
-% parfor (ix = 1:numel(group_dir) * numel(para_dir), parforArg)
-for ix = 1%:numel(group_dir) * numel(para_dir)
+parfor (ix = 1:numel(group_dir) * numel(para_dir), parforArg)
+% for ix = 1:numel(group_dir) * numel(para_dir)
     %get sub-index S from global index G by modulo. Loop order is not as for 
     %nested loops, but parfor mixes order anyway. First is group index:
     gix = mod(ix - 1, numel(group_dir)) + 1;
     if parforArg == 0
-        sbj_filt = grpXsbj_filt{gix}; %%#ok<PFBNS>
+        sbj_filt = grpXsbj_filt{gix}; %#ok<PFBNS>
     else
         sbj_filt = [];
     end
@@ -96,7 +96,7 @@ for ix = 1%:numel(group_dir) * numel(para_dir)
                @nefi_pipe3A,...
                @nefi_pipe3B,...
                @nefi_peekpipe};
-    runps = 2:6;
+    runps = 1:6;
     %You can also run only a subset of pipes, e.g. 2:length(pipeArr)
 
 
