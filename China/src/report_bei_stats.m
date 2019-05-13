f0d8f5f5238b288cf507a846d3e7864985d2557c
zenBen <ben.cowley@helsinki.fi>
2019-05-13 17:03:02 +0300

    report_bei_stats as Function.

--- China/src/report_bei_stats.m
+++ China/src/report_bei_stats.m
@@ -1,3 +1,5 @@
+function [treeStats, treeRej, bestpipe] = report_bei_stats(anew, plvls)
+
 %% INIT
 %make paths
 % name = 'project_NEUROENHANCE';
@@ -12,12 +14,14 @@ if ~isfolder(oud), mkdir(oud); end
 %specify groups, protocols, and pipe levels
 grps = {'Control'  'English'  'Music'};
 cnds = {'atten' 'AV' 'multi' 'melody'};
-plvls = {{'2A' '2B' '2C'}; {'3A' '3B'}; {'epout'}};
-plotnsave = false;
+if nargin < 2
+    plvls = {{'2A' '2B' '2C'}; {'3A' '3B'}; {'epout'}};
+end
 
 % READ SUBJxGROUP INFO
 if exist(fullfile(oud, 'subjectXgroup.mat'), 'file') == 2
-    load(fullfile(oud, 'subjectXgroup.mat'))
+    sbjXgrp = load(fullfile(oud, 'subjectXgroup.mat'));
+    sbjXgrp = sbjXgrp.sbjXgrp;
 else
     %read list of subjects per group
     sbjXgrp = map_bei_subj_grps;
@@ -26,21 +30,21 @@ end
 
 
 %% CALL FUNCTIONS TO READ & PROCESS STATS LOGS
-[treeStats, peek_stat_files] = ctap_get_peek_stats(ind, oud...
+[treeStats, peek_stat_files] = ctap_get_peek_stats(ind, oud, 'anew', anew...
                                     , 'post_pipe_part', 'peekpipe/this/');
 [treeStats, new_rows] = ctap_compare_branchstats(treeStats, grps, cnds...
                                                     , plvls(1:2), 1:9, 1:9);
 
 
 %% CALL FUNCTIONS TO READ & PROCESS REJECTION LOGS
-[treeRej, rej_files] = ctap_get_rejections(ind, oud...
+[treeRej, rej_files] = ctap_get_rejections(ind, oud, 'anew', anew...
                         , 'post_pipe_part', 'this/logs/all_rejections.txt');
 treeRej = ctap_parse_rejections(treeRej, grps, cnds, 1:9);
 treeRej = ctap_compare_branch_rejs(treeRej, grps, cnds, plvls);
 
 
 %% JUDGEMENT : THE COMBININING
-bestpipe = ctap_get_bestpipe(treeStats, treeRej, oud, plvls);
+bestpipe = ctap_get_bestpipe(treeStats, treeRej, oud, plvls, 'anew', anew);
 
 
 %% GROUP-WISE AND CONDITION-WISE HISTOGRAMS OF PIPE STATS
